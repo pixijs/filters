09b3152bcdbaa8e4c68f8387d64ca2c29a808530
Matt Karl <matt.karl@jibo.com>
2016-08-08 19:04:03 -0400

    Auto-generated commit

--- bin/filters.js
+++ bin/filters.js
@@ -1,6 +1,6 @@
 /*!
  * pixi-filters - v1.0.1
- * Compiled Mon Aug 08 2016 18:57:44 GMT-0400 (EDT)
+ * Compiled Mon Aug 08 2016 19:03:40 GMT-0400 (EDT)
  *
  * pixi-filters is licensed under the MIT License.
  * http://www.opensource.org/licenses/mit-license
