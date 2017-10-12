0bb0ad35830d11c112bd8b6b7ff0ffac8a531490
finscn <fins@163.com>
2017-10-12 07:47:29 -0500

    Fix bugs of ZoomBlure about resolution & size (#58)
    * fix bugs of ZoomBlure about resolution & size
    
    * remove `apply`
    
    * remove blank line
    
    * Update index.html
    
    * Removed typings for setSize and clearSize

--- filters/all/types.d.ts
+++ filters/all/types.d.ts
@@ -101,8 +101,6 @@ declare namespace PIXI.filters {
         center:PIXI.Point|number[];
         innerRadius:number;
         radius:number;
-        setSize(width:number, height:number):void;
-        clearSize():void;
     }
 }
 
--- filters/zoom-blur/src/ZoomBlurFilter.js
+++ filters/zoom-blur/src/ZoomBlurFilter.js
@@ -11,59 +11,17 @@ import fragment from './zoom-blur.frag';
  * @param {number} [strength=2] Sets the strength of the zoom blur effect
  * @param {PIXI.Point|number[]} [center=[0,0]] The center of the zoom.
  * @param {number} [innerRadius=0] The inner radius of zoom. The part in inner circle won't apply zoom blur effect.
- * @param {number} [radius=1E9] Very very large outer radius of the zoom.
+ * @param {number} [radius=1E8] Outer radius of the zoom, The default value is very very large.
  */
 export default class ZoomBlurFilter extends PIXI.Filter
 {
-    constructor(strength = 0.1, center = [0, 0], innerRadius = 0, radius = 1E9) {
+    constructor(strength = 0.1, center = [0, 0], innerRadius = 0, radius = 1E8) {
         super(vertex, fragment);
 
         this.center = center;
         this.strength = strength;
         this.innerRadius = innerRadius;
         this.radius = radius;
-
-        this._defaultSize = new Float32Array([0, 0]);
-        this._size = new Float32Array([0, 0]);
-        this.clearSize();
-    }
-
-    /**
-     * Apply the filter
-     * @override
-     * @private
-     */ 
-    apply(filterManager, input, output, clear)
-    {
-        // Apply the input source if view is not set
-        if (!this._customSize)
-        {
-            this._defaultSize[0] = input.sourceFrame.width;
-            this._defaultSize[1] = input.sourceFrame.height;
-        }
-        filterManager.applyFilter(this, input, output, clear);
-    }
-
-    /**
-     * Set the view size, if defining a region.
-     * @method
-     * @param {number} width - Width of the view size
-     * @param {number} height - Height of the view size
-     */
-    setSize(width, height) {
-        this._customSize = true;
-        this._size[0] = width;
-        this._size[1] = height;
-        this.uniforms.uViewSize = this._size;
-    }
-
-    /**
-     * Clear the defined view size of the region to effect.
-     * @method
-     */
-    clearSize() {
-        this._customSize = false;
-        this.uniforms.uViewSize = this._defaultSize;
     }
 
     /**
