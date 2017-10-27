4419da903e759b035682bf01135bfa610fc9408d
finscn <fins@163.com>
2017-10-27 23:51:01 +0800

    update example

--- tools/demo/src/filters/godray.js
+++ tools/demo/src/filters/godray.js
@@ -1,8 +1,24 @@
 export default function() {
-    this.addFilter('GodrayFilter', function(folder) {
-        folder.add(this, 'time', 0, 1);
-        folder.add(this, 'angle', -60, 60);
-        folder.add(this, 'gain', 0, 1);
-        folder.add(this, 'lacunarity', 0, 5);
+    const app = this;
+
+    this.addFilter('GodrayFilter', {
+        enabled: false,
+        opened: false,
+        oncreate: function(folder) {
+            const filter = this;
+
+            this.angle = 30;
+            this.gain = 0.5;
+            this.lacunarity = 2.5;
+
+            app.events.on('animate', function(){
+                filter.time += app.ticker.elapsedMS / 1000;
+            });
+
+            folder.add(this, 'time', 0, 1);
+            folder.add(this, 'angle', -60, 60);
+            folder.add(this, 'gain', 0, 1);
+            folder.add(this, 'lacunarity', 0, 5);
+        }
     });
 }
