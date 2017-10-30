328091ea8bb622f51d2813b5eea93445fc9f06f5
finscn <fins@163.com>
2017-10-30 09:30:35 +0800

    Refactor `GodrayFilter` for optimizing. (#91)
    * Refactor GodrayFilter for optimizing
    
    * `time` as property , access directly , because in real game, `time` will be changed every game tick usually.
    * use `get/set angle` . because this property. because in real game, `angle` won't be changed every game tick usually.
    * simplify shader ( use filterArea-coord ), and fix aspect ratio via `filterArea.y / filterArea.x`. 
       I know some filters must use `input.sourceFrame.size`, but this filter is not. use filterArea-coord is good enough. 
    * use `${perlin}` instead of `$perlin` , make it as same as js-template-string.
    
    * Update main.frag
    
    * fix the bug about `angle`
    
    * Update main.frag
    
    * Update GodrayFilter.js
    
    * Update GodrayFilter.js
    
    * adjust default speed of the filter
    
    * update example
    
    * add `play manually`

--- filters/godray/src/main.frag
+++ filters/godray/src/main.frag
@@ -1,22 +1,26 @@
 varying vec2 vTextureCoord;
-uniform float time;
-uniform vec2 angleDir;
-uniform float gain;
-uniform float lacunarity;
 uniform sampler2D uSampler;
-
 uniform vec4 filterArea;
 uniform vec2 dimensions;
 
-$perlin
+uniform vec2 angleDir;
+uniform float gain;
+uniform float lacunarity;
+uniform float time;
+
+${perlin}
 
 void main(void) {
     gl_FragColor = texture2D(uSampler, vTextureCoord);
-    vec2 coord = vTextureCoord * vec2(filterArea.x / dimensions.x, filterArea.y / dimensions.y);
+    vec2 coord = vTextureCoord * filterArea.xy / dimensions.xy;
+
     float xx = angleDir.x;
     float yy = angleDir.y;
-    vec3 dir = vec3((xx * coord.x) + (yy * coord.y), (xx * coord.x) + (yy * coord.y), 0.0);
-    float noise = turb(dir + vec3(time, 0.0, 62.1 + time), vec3(480.0, 320.0, 480.0), lacunarity, gain);
+
+    float d = (xx * coord.x) + (yy * coord.y);
+    vec3 dir = vec3(d, d, 0.0);
+
+    float noise = turb(dir + vec3(time, 0.0, 62.1 + time) * 0.05, vec3(480.0, 320.0, 480.0), lacunarity, gain);
     noise = mix(noise, 0.0, 0.3);
     //fade vertically.
     vec4 mist = vec4(noise, noise, noise, 1.0) * (1.0 - coord.y);
--- tools/demo/src/filters/godray.js
+++ tools/demo/src/filters/godray.js
@@ -1,8 +1,36 @@
 export default function() {
-    this.addFilter('GodrayFilter', function(folder) {
-        folder.add(this, 'time', 0, 1);
-        folder.add(this, 'angle', -60, 60);
-        folder.add(this, 'gain', 0, 1);
-        folder.add(this, 'lacunarity', 0, 5);
+    const app = this;
+
+    this.addFilter('GodrayFilter', {
+        enabled: false,
+        opened: false,
+        oncreate: function(folder) {
+            const filter = this;
+
+            this.angle = 30;
+            this.gain = 0.5;
+            this.lacunarity = 2.5;
+
+            filter.manually = false;
+
+            app.events.on('toggle', function(enabled) {
+                filter.disabled = !enabled;
+                if (enabled && !filter.manually) {
+                    filter.time = 0;
+                }
+            });
+
+            app.events.on('animate', function() {
+                if (!filter.manually){
+                    filter.time += app.ticker.elapsedMS / 1000;
+                }
+            });
+
+            folder.add(this, 'manually').name(' * play manually');
+            folder.add(this, 'time', 0, 1);
+            folder.add(this, 'angle', -60, 60);
+            folder.add(this, 'gain', 0, 1);
+            folder.add(this, 'lacunarity', 0, 5);
+        }
     });
 }
