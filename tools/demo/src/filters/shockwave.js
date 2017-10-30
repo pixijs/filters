7a305ca89fdb72964a962274a42e59e1047f9d6c
finscn <fins@163.com>
2017-10-30 09:28:29 +0800

    Refactor ShockwaveFilter (#95)
    * refactor ShockwaveFilter
    
    * Update config.json
    
    * Update config.json
    
    * Update config.json
    
    * update config for screenshots

--- tools/demo/src/filters/shockwave.js
+++ tools/demo/src/filters/shockwave.js
@@ -1,7 +1,39 @@
 export default function() {
-    this.addFilter('ShockwaveFilter', function(folder) {
-        folder.add(this, 'time', 0, 1);
-        folder.add(this.center, '0', 0, 1).name('center.x');
-        folder.add(this.center, '1', 0, 1).name('center.y');
+    const app = this;
+
+    this.addFilter('ShockwaveFilter', {
+        enabled: false,
+        global: false,
+        args: [[app.initWidth / 2, app.initHeight / 2]],
+        oncreate(folder) {
+
+            const filter = this;
+            const maxTime = 2.5;
+
+            filter.manually = false;
+
+            app.events.on('toggle', function(enabled) {
+                filter.disabled = !enabled;
+                if (enabled && !filter.manually) {
+                    filter.time = 0;
+                }
+            });
+
+            app.events.on('animate', function() {
+                if (!filter.manually){
+                    filter.time += app.ticker.elapsedMS / 1000;
+                    filter.time %= maxTime;
+                }
+            });
+
+            folder.add(this, 'manually').name(' * play manually');
+            folder.add(this, 'time', 0, maxTime);
+            folder.add(this, 'amplitude', 1, 100);
+            folder.add(this, 'wavelength', 2, 400);
+            folder.add(this, 'brightness', 0.2, 2.0);
+            folder.add(this, 'radius', 100, 2000);
+            folder.add(this.center, '0', 0, app.initWidth).name('center.x');
+            folder.add(this.center, '1', 0, app.initHeight).name('center.y');
+        }
     });
 }
