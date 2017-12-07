67bdad052f8633d1280f8984cde3829c97743557
finscn <fins@163.com>
2017-12-07 22:14:33 +0800

    Update index.js

--- bundle/src/index.js
+++ bundle/src/index.js
@@ -1,5 +1,5 @@
-export {AsciiFilter} from '@pixi/filter-ascii';
 export {AdvancedBloomFilter} from '@pixi/filter-advanced-bloom';
+export {AsciiFilter} from '@pixi/filter-ascii';
 export {BloomFilter} from '@pixi/filter-bloom';
 export {BulgePinchFilter} from '@pixi/filter-bulge-pinch';
 export {ColorReplaceFilter} from '@pixi/filter-color-replace';
@@ -10,13 +10,16 @@ export {DropShadowFilter} from '@pixi/filter-drop-shadow';
 export {EmbossFilter} from '@pixi/filter-emboss';
 export {GlowFilter} from '@pixi/filter-glow';
 export {GodrayFilter} from '@pixi/filter-godray';
-export {OutlineFilter} from '@pixi/filter-outline';
+export {KawaseBlurFilter} from '@pixi/filter-kawase-blur';
+export {MotionBlurFilter} from '@pixi/filter-motion-blur';
 export {MultiColorReplaceFilter} from '@pixi/filter-multi-color-replace';
+export {OldFilmFilter} from '@pixi/filter-old-film';
+export {OutlineFilter} from '@pixi/filter-outline';
 export {PixelateFilter} from '@pixi/filter-pixelate';
 export {RGBSplitFilter} from '@pixi/filter-rgb-split';
+export {RadialBlurFilter} from '@pixi/filter-radial-blur';
 export {ShockwaveFilter} from '@pixi/filter-shockwave';
 export {SimpleLightmapFilter} from '@pixi/filter-simple-lightmap';
 export {TiltShiftFilter, TiltShiftAxisFilter, TiltShiftXFilter, TiltShiftYFilter} from '@pixi/filter-tilt-shift';
 export {TwistFilter} from '@pixi/filter-twist';
 export {ZoomBlurFilter} from '@pixi/filter-zoom-blur';
-
