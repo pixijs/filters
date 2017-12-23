f31e41c52e06b80c37da4afce87b3f9e3536e483
finscn <finscn@gmail.com>
2017-12-23 22:15:28 +0800

    Improve OutlineFilter quality (#116)

--- tools/demo/src/filters/outline.js
+++ tools/demo/src/filters/outline.js
@@ -1,8 +1,13 @@
 export default function() {
     this.addFilter('OutlineFilter', {
+        enabled: false,
         fishOnly: true,
+        args: [4, 0x0, 0.25],
         oncreate(folder) {
-            folder.add(this, 'thickness', 0, 20);
+            this.padding = this.thickness + 4;
+            folder.add(this, 'thickness', 0, 10).onChange((value) => {
+                this.padding = value + 4;
+            });
             folder.addColor(this, 'color');
         }
     });
