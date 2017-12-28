cc8368fe8a919e7bf59eb8575ce501847d99d6de
finscn <finscn@gmail.com>
2017-12-29 07:47:23 +0800

    update example

--- tools/demo/src/filters/adjustment.js
+++ tools/demo/src/filters/adjustment.js
@@ -4,7 +4,7 @@ export default function() {
         enabled: false,
         global: false,
         opened: false,
-        fishOnly: true,
+        fishOnly: false,
         // args: [[app.initWidth / 2, app.initHeight / 2]],
         oncreate(folder) {
             folder.add(this, 'gamma', 0, 10);
