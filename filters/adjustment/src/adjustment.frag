fbfbd1881335f661fda5f177b66bf593b1d2c792
finscn <finscn@gmail.com>
2017-12-29 07:58:33 +0800

    update example

--- filters/adjustment/src/adjustment.frag
+++ filters/adjustment/src/adjustment.frag
@@ -14,18 +14,18 @@ void main(void)
 {
     vec4 c = texture2D(uSampler, vTextureCoord);
 
-    float k = c.a > 0.0 ? c.a : 1.0;
+    if (c.a > 0.0) {
+        c.rgb /= c.a;
 
-    c.rgb /= k;
+        vec3 rgb = pow(c.rgb, vec3(1. / gamma));
+        rgb = mix(vec3(.5), mix(vec3(dot(vec3(.2125, .7154, .0721), rgb)), rgb, saturation), contrast);
+        rgb.r *= red;
+        rgb.g *= green;
+        rgb.b *= blue;
+        c.rgb = rgb * brightness;
 
-    vec3 rgb = pow(c.rgb, vec3(1. / gamma));
-    rgb = mix(vec3(.5), mix(vec3(dot(vec3(.2125, .7154, .0721), rgb)), rgb, saturation), contrast);
-    rgb.r *= red;
-    rgb.g *= green;
-    rgb.b *= blue;
-    c.rgb = rgb * brightness;
-
-    c.rgb *= k;
+        c.rgb *= c.a;
+    }
 
     gl_FragColor = c * alpha;
 }
--- tools/demo/src/filters/adjustment.js
+++ tools/demo/src/filters/adjustment.js
@@ -4,8 +4,7 @@ export default function() {
         enabled: false,
         global: false,
         opened: false,
-        fishOnly: false,
-        // args: [[app.initWidth / 2, app.initHeight / 2]],
+        fishOnly: true,
         oncreate(folder) {
             folder.add(this, 'gamma', 0, 10);
             folder.add(this, 'saturation', 0, 10);
@@ -15,14 +14,6 @@ export default function() {
             folder.add(this, 'green', 0, 10);
             folder.add(this, 'blue', 0, 10);
             folder.add(this, 'alpha', 0, 1);
-            // folder.add(this, 'noise', 0, 1);
-            // folder.add(this, 'noiseSize', 1, 10);
-            // folder.add(this, 'scratch', -1, 1);
-            // folder.add(this, 'scratchDensity', 0, 1);
-            // folder.add(this, 'scratchWidth', 1, 20);
-            // folder.add(this, 'vignetting', 0, 1);
-            // folder.add(this, 'vignettingAlpha', 0, 1);
-            // folder.add(this, 'vignettingBlur', 0, 1);
         }
     });
 }
