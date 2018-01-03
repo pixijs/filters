909cd0128b3bc58d2cea9c883525565f484e7cec
finscn <finscn@gmail.com>
2018-01-04 00:40:44 +0800

    Update AdjustmentFilter.js

--- filters/adjustment/src/AdjustmentFilter.js
+++ filters/adjustment/src/AdjustmentFilter.js
@@ -10,34 +10,75 @@ import fragment from './adjustment.frag';
  * @extends PIXI.Filter
  * @memberof PIXI.filters
  *
- * @param {object|number} [options] - The optional parameters of old film effect.
- *                        When options is a number , it will be `seed`
- * @param {number} [options.sepia=0.3] - The amount of saturation of sepia effect,
- *        a value of `1` is more saturation and closer to `0` is less, and a value of
- *        `0` produces no sepia effect
- * @param {number} [options.noise=0.3] - Opacity/intensity of the noise effect between `0` and `1`
- * @param {number} [options.noiseSize=1.0] - The size of the noise particles
- * @param {number} [options.scratch=0.5] - How often scratches appear
- * @param {number} [options.scratchDensity=0.3] - The density of the number of scratches
- * @param {number} [options.scratchWidth=1.0] - The width of the scratches
- * @param {number} [options.vignetting=0.3] - The radius of the vignette effect, smaller
- *        values produces a smaller vignette
- * @param {number} [options.vignettingAlpha=1.0] - Amount of opacity of vignette
- * @param {number} [options.vignettingBlur=0.3] - Blur intensity of the vignette
- * @param {number} [seed=0] - A see value to apply to the random noise generation
+ * @param {object|number} [options] - The optional parameters of the filter.
+ * @param {number} [options.gamma=1] - TODO
+ * @param {number} [options.saturation=1] - TODO
+ * @param {number} [options.contrast=1] - TODO
+ * @param {number} [options.brightness=1] - TODO
+ * @param {number} [options.red=1] - TODO
+ * @param {number} [options.green=1] - TODO
+ * @param {number} [options.blue=1] - TODO
+ * @param {number} [options.alpha=1] - TODO
  */
 export default class AdjustmentFilter extends PIXI.Filter {
     constructor(options) {
         super(vertex, fragment);
 
         Object.assign(this, {
+            /**
+             * The amount of Gamma
+             * @member {number} gamma
+             * @default 1
+             */
             gamma: 1,
+
+            /**
+             * The amount of saturation
+             * @member {number} saturation
+             * @default 1
+             */
             saturation: 1,
+
+            /**
+             * The amount of contrast
+             * @member {number} contrast
+             * @default 1
+             */
             contrast: 1,
+
+            /**
+             * The amount of brightness
+             * @member {number} brightness
+             * @default 1
+             */
             brightness: 1,
+
+            /**
+             * The amount of Red Channel
+             * @member {number} red
+             * @default 1
+             */
             red: 1,
+
+            /**
+             * The amount of Green Channel
+             * @member {number} green
+             * @default 1
+             */
             green: 1,
+
+            /**
+             * The amount of Blue Channel
+             * @member {number} blue
+             * @default 1
+             */
             blue: 1,
+
+            /**
+             * The amount of Alpha Channel
+             * @member {number} alpha
+             * @default 1
+             */
             alpha: 1,
         }, options);
     }
