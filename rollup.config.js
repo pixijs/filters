d8b13a8aa1d58d60b4d166073b81f1b9a6fc421a
Matt Karl <mattkarl@netflix.com>
2019-05-10 15:30:46 -0700

    Bump the rollup-string plugin

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import fs from 'fs';
 import buble from 'rollup-plugin-buble';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import string from 'rollup-plugin-string';
+import { string } from 'rollup-plugin-string';
 import { terser } from 'rollup-plugin-terser';
 import minimist from 'minimist';
 import MagicString from 'magic-string';
