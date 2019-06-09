1f379cb4d5306927c6f578f5b9dbc5bca2d4e47a
Michael Deal <mudcube@gmail.com>
2019-06-08 23:59:12 -0700

    Update filters/color-overlay/types.d.ts
    Co-Authored-By: Matt Karl <matt@mattkarl.com>

--- filters/color-overlay/types.d.ts
+++ filters/color-overlay/types.d.ts
@@ -3,7 +3,6 @@ declare module "@pixi/filter-color-overlay" {
     export class ColorOverlayFilter extends PIXI.Filter {
         constructor(color?:number|[number, number, number]);
         epsilon:number;
-        originalColor:number|number[];
         color:number|[number, number, number];
     }
 }
