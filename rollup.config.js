51969c03f5adddc5c0e92f1e88894114556adf1f
Julien Mourer <getkey@users.noreply.github.com>
2020-02-05 00:09:01 +0100

    Import PixiJS as a peer dependency (#221)

--- rollup.config.js
+++ rollup.config.js
@@ -140,7 +140,10 @@ async function main() {
         const freeze = false;
 
         // Generate the externals to use, by default don't include dependencies
-        const external = Object.keys(pkg.dependencies);
+        const external = [].concat(
+            Object.keys(pkg.dependencies || {}),
+            Object.keys(pkg.peerDependencies || {})
+        );
 
         results.push({
             input,
