ed010731ab4f7d66e171ac3a825d76137f758ed1
Matt Karl <matt@mattkarl.com>
2020-02-05 18:17:23 -0800

    Convert GlowFilter to use options

--- filters/glow/src/GlowFilter.js
+++ filters/glow/src/GlowFilter.js
@@ -5,30 +5,38 @@ import {rgb2hex, hex2rgb} from '@pixi/utils';
 
 /**
  * GlowFilter, originally by mishaa
- * http://www.html5gamedevs.com/topic/12756-glow-filter/?hl=mishaa#entry73578
- * http://codepen.io/mishaa/pen/raKzrm<br>
+ * [codepen]{@link http://codepen.io/mishaa/pen/raKzrm}.<br>
  * ![original](../tools/screenshots/dist/original.png)![filter](../tools/screenshots/dist/glow.png)
- *
  * @class
  *
  * @extends PIXI.Filter
  * @memberof PIXI.filters
  * @see {@link https://www.npmjs.com/package/@pixi/filter-glow|@pixi/filter-glow}
  * @see {@link https://www.npmjs.com/package/pixi-filters|pixi-filters}
- * @param {number} [distance=10] The distance of the glow. Make it 2 times more for resolution=2. It cant be changed after filter creation.
- * @param {number} [outerStrength=4] The strength of the glow outward from the edge of the sprite.
- * @param {number} [innerStrength=0] The strength of the glow inward from the edge of the sprite.
- * @param {number} [color=0xffffff] The color of the glow.
- * @param {number} [quality=0.1] A number between 0 and 1 that describes the quality of the glow.
+ * @param {number} [options] Options for glow.
+ * @param {number} [options.distance=10] The distance of the glow. Make it 2 times more for resolution=2.
+ *        It can't be changed after filter creation.
+ * @param {number} [options.outerStrength=4] The strength of the glow outward from the edge of the sprite.
+ * @param {number} [options.innerStrength=0] The strength of the glow inward from the edge of the sprite.
+ * @param {number} [options.color=0xffffff] The color of the glow.
+ * @param {number} [options.quality=0.1] A number between 0 and 1 that describes the quality of the glow.
+ *        The higher the number the less performant.
  *
  * @example
  *  someSprite.filters = [
- *      new GlowFilter(15, 2, 1, 0xFF0000, 0.5)
+ *      new GlowFilter({ distance: 15, outerStrength: 2 })
  *  ];
  */
 class GlowFilter extends Filter {
 
-    constructor(distance = 10, outerStrength = 4, innerStrength = 0, color = 0xffffff, quality = 0.1) {
+    constructor(options) {
+        let {
+            distance,
+            outerStrength,
+            innerStrength,
+            color,
+            quality } = Object.assign({}, GlowFilter.defaults, options);
+
         distance = Math.round(distance);
 
         super(vertex, fragment
@@ -36,11 +44,13 @@ class GlowFilter extends Filter {
             .replace(/__DIST__/gi, distance.toFixed(0) + '.0'));
 
         this.uniforms.glowColor = new Float32Array([0, 0, 0, 1]);
-        this.color = color;
-        this.outerStrength = outerStrength;
-        this.innerStrength = innerStrength;
 
-        this.padding = distance;
+        Object.assign(this, {
+            color,
+            outerStrength,
+            innerStrength,
+            padding: distance,
+        });
     }
 
     /**
@@ -79,5 +89,13 @@ class GlowFilter extends Filter {
         this.uniforms.innerStrength = value;
     }
 }
+
+GlowFilter.defaults = {
+    distance: 10,
+    outerStrength: 4,
+    innerStrength: 0,
+    color: 0xffffff,
+    quality: 0.1,
+};
 
 export { GlowFilter };
