1a80e8233380d114d44e0e673e3882b20fa9552e
Matt Karl <matt@mattkarl.com>
2020-02-16 15:25:30 -0800

    Better type for generic point

--- filters/zoom-blur/types.d.ts
+++ filters/zoom-blur/types.d.ts
@@ -2,15 +2,15 @@
 declare module "@pixi/filter-zoom-blur" {
     export interface ZoomBlurFilterOptions {
         strength:number;
-        center:PIXI.Point|number[];
+        center:PIXI.Point|[number, number];
         innerRadius:number;
         radius:number;
     }
     export class ZoomBlurFilter extends PIXI.Filter {
         constructor(options?:ZoomBlurFilterOptions);
-        constructor(strength?:number, center?:PIXI.Point|number[], innerRadius?:number, radius?:number);
+        constructor(strength?:number, center?:PIXI.Point|[number, number], innerRadius?:number, radius?:number);
         strength:number;
-        center:PIXI.Point|number[];
+        center:PIXI.Point|[number, number];
         innerRadius:number;
         radius:number;
     }
