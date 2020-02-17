01cb5a201afacafc29129258e2fbdd3ce1835f7c
Matt Karl <matt@mattkarl.com>
2020-02-17 15:15:37 -0800

    Made options optional

--- filters/bulge-pinch/types.d.ts
+++ filters/bulge-pinch/types.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="pixi.js" />
 declare module "@pixi/filter-bulge-pinch" {
     export interface BulgePinchFilterOptions {
-        center:PIXI.Point|[number, number];
-        radius:number;
-        strength:number;
+        center?:PIXI.Point|[number, number];
+        radius?:number;
+        strength?:number;
     }
     export class BulgePinchFilter extends PIXI.Filter {
         constructor(options?:BulgePinchFilterOptions);
--- filters/zoom-blur/types.d.ts
+++ filters/zoom-blur/types.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="pixi.js" />
 declare module "@pixi/filter-zoom-blur" {
     export interface ZoomBlurFilterOptions {
-        strength:number;
-        center:PIXI.Point|[number, number];
-        innerRadius:number;
-        radius:number;
+        strength?:number;
+        center?:PIXI.Point|[number, number];
+        innerRadius?:number;
+        radius?:number;
     }
     export class ZoomBlurFilter extends PIXI.Filter {
         constructor(options?:ZoomBlurFilterOptions);
