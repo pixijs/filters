757d6ba415d043ce748236923d1fc4f62b9caab6
Matt Karl <mattkarl@netflix.com>
2020-04-03 13:49:31 -0700

    Add auto-padding based on the outline thickness

--- tools/demo/src/filters/outline.js
+++ tools/demo/src/filters/outline.js
@@ -4,10 +4,7 @@ export default function() {
         fishOnly: true,
         args: [4, 0x0, 0.25],
         oncreate(folder) {
-            this.padding = this.thickness + 4;
-            folder.add(this, 'thickness', 0, 10).onChange((value) => {
-                this.padding = value + 4;
-            });
+            folder.add(this, 'thickness', 0, 10);
             folder.addColor(this, 'color');
         }
     });
