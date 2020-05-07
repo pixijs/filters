9b37cc21c5d47043f5d4e0cdb4f9cb135d67787a
jrod-disco <jrod.disco@gmail.com>
2020-05-07 11:37:35 -0400

    Additional cleanup for shiny PR

--- filters/godray/types.d.ts
+++ filters/godray/types.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="pixi.js" />
 declare namespace PIXI.filters {
     export class GodrayFilter extends PIXI.Filter {
-<<<<<<< HEAD
         constructor(options?:GodrayFilterOptions);
         angle:number;
         center:PIXI.Point|Array<number>;
@@ -19,25 +18,6 @@ declare namespace PIXI.filters {
         lacunarity:number;
         time:number;
         alpha:number;
-=======
-        constructor(options?: GodrayFilterOptions);
-        angle: number;
-        center: PIXI.Point | Array<number>;
-        parallel: boolean;
-        gain: number;
-        lacunarity: number;
-        time: number;
-        alpha: number;
-    }
-    export interface GodrayFilterOptions {
-        angle: number;
-        center: PIXI.Point | Array<number>;
-        parallel: boolean;
-        gain: number;
-        lacunarity: number;
-        time: number;
-        alpha: number;
->>>>>>> 550fd2bc1d343dcc6789cd3a62c51602b614368f
     }
 }
 
