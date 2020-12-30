3cae7861ced0ac73518c59bae9b476860f94e365
Matt Karl <matt@mattkarl.com>
2020-12-29 19:24:19 -0800

    Add constructor options for TwistFilter (#283)

--- filters/twist/types.d.ts
+++ filters/twist/types.d.ts
@@ -1,7 +1,18 @@
 /// <reference types="pixi.js" />
+
 declare namespace PIXI.filters {
+    export interface TwistFilterOptions {
+        angle?: number;
+        offset?: PIXI.Point;
+        padding?: number;
+        radius?: number;
+    }
     export class TwistFilter extends PIXI.Filter {
-        constructor(radius?:number, angle?:number, padding?:number);
+        /**
+         * @deprecated Use options instead.
+         */
+        constructor(radius:number, angle?:number, padding?:number);
+        constructor(options?:TwistFilterOptions);
         angle:number;
         offset:PIXI.Point;
         radius:number;
