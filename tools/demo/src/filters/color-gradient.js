6a876d333ba7517b6f778d3e41251d5a10246a70
Viktor Persson <viktor.persson@arcsin.se>
2022-12-27 12:07:44 +0100

    Update demo controls; add CSS gradient controller and reset button.

--- tools/demo/src/filters/color-gradient.js
+++ tools/demo/src/filters/color-gradient.js
@@ -9,33 +9,69 @@ export default function ()
     this.addFilter('ColorGradientFilter', {
         enabled: true,
         fishOnly: true,
-        opened: true,
         args: [{ stops }],
         oncreate(folder)
         {
-            folder.add(this, 'type', { LINEAR: 0, RADIAL: 1, CONIC: 2 });
+            const removeColorStopControllers = () =>
+            {
+                for (let i = folder.controllers.length - 1; i > 0; i--)
+                {
+                    if (folder.controllers[i]._name.includes('stops['))
+                    {
+                        folder.controllers[i].destroy();
+                    }
+                }
+            };
 
             const onStopChange = () =>
             {
                 this.stops = stops;
             };
 
-            for (let i = 0; i < this.stops.length; i++)
+            const createColorStopControllers = (stops) =>
             {
-                folder.addColor(stops[i], 'color')
-                    .name(`stops[${i}].color`)
-                    .onChange(onStopChange);
-                folder.add(stops[i], 'offset', 0, 1)
-                    .name(`stops[${i}].offset`)
-                    .onChange(onStopChange);
-                folder.add(stops[i], 'alpha', 0, 1)
-                    .name(`stops[${i}].alpha`)
-                    .onChange(onStopChange);
-            }
+                removeColorStopControllers();
 
+                for (let i = 0; i < this.stops.length; i++)
+                {
+                    folder.addColor(stops[i], 'color')
+                        .name(`stops[${i}].color`)
+                        .onChange(onStopChange);
+                    folder.add(stops[i], 'offset', 0, 1)
+                        .name(`stops[${i}].offset`)
+                        .onChange(onStopChange);
+                    folder.add(stops[i], 'alpha', 0, 1)
+                        .name(`stops[${i}].alpha`)
+                        .onChange(onStopChange);
+                }
+            };
+
+            // handle css gradient
+            const onCssGradientChange = (value) =>
+            {
+                console.log('css gradient changed..');
+            };
+
+            // init
+            const applyDefaultOptions = () =>
+            {
+                folder.reset();
+                createColorStopControllers(stops);
+            };
+
+            const misc = {
+                'reset options': applyDefaultOptions,
+                cssGradient: '',
+            };
+
+            folder.add(misc, 'reset options');
+            folder.add(misc, 'cssGradient').name('from CSS gradient').onChange(onCssGradientChange);
+            folder.add(this, 'type', { LINEAR: 0, RADIAL: 1, CONIC: 2 });
             folder.add(this, 'alpha', 0, 1);
             folder.add(this, 'angle', -180, 180, 1);
             folder.add(this, 'maxColors', 0, 24, 1);
+
+            applyDefaultOptions(stops);
         },
     });
 }
