ed2a10b1f69498ae8a8cac95eb5259237db2d162
Viktor Persson <viktor.persson@arcsin.se>
2022-12-28 14:59:50 +0100

    Add more tests cases for utils.colorToNormalizedRgba().

--- filters/color-gradient/test/utils.test.ts
+++ filters/color-gradient/test/utils.test.ts
@@ -6,20 +6,25 @@ describe.only('utils', () =>
     test.concurrent('colorToNormalizedRgba', async () =>
     {
         const rgbaRed = [1, 0, 0, 1];
-        const transparentRed = [1, 0, 0, 0];
+        const semiTransparentRed = [1, 0, 0, 0.5];
 
         const testCases = {
-            red: rgbaRed,
-            '#ff0000': rgbaRed,
-            '#ff000000': transparentRed,
-            '#f00': rgbaRed,
-            'rgb(255,0,0)': rgbaRed,
-            'rgba(255,0,0)': rgbaRed,
-            'rgba(255,0,0,1)': rgbaRed,
-            'rgba(255,0,0,100%)': rgbaRed,
-            'rgba(255,0,0,0%)': transparentRed,
-            'rgba(255,0,0,0)': transparentRed,
             transparent: [0, 0, 0, 0],
+            red: rgbaRed,
+            '#ff000000': [1, 0, 0, 0],
+            '#ff0000': rgbaRed,
+            '#f00': rgbaRed,
+            'rgb(255, 0, 0)': rgbaRed,
+            'rgba(255, 0, 0)': rgbaRed,
+            'rgba(255, 0, 0, 0.5)': semiTransparentRed,
+            'rgba(255, 0, 0 / 0.5)': semiTransparentRed,
+            'rgba(255, 0, 0, 50%)': semiTransparentRed,
+            'rgba(255, 0, 0 / 50%)': semiTransparentRed,
+            'hsl(0, 100%, 50%)': rgbaRed,
+            'hsla(0, 100%, 50%, 0.5)': semiTransparentRed,
+            'hsla(0, 100%, 50% / 0.5)': semiTransparentRed,
+            'hsla(0, 100%, 50%, 50%)': semiTransparentRed,
+            'hsla(0, 100%, 50% / 50%)': semiTransparentRed,
         };
 
         for (const [value, expectedResult] of Object.entries(testCases))
