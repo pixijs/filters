e055089ffcaad41009b389947fc41498545ae87e
Viktor Persson <vikpe@users.noreply.github.com>
2023-01-25 14:26:39 +0100

    Add ColorGradientFilter (#362)

---
+++ filters/color-gradient/src/ColorGradientFilter.ts
@@ -0,0 +1,184 @@
+import fragment from './colorGradient.frag';
+import vertex from './colorGradient.vert';
+import { Filter } from '@pixi/core';
+import { parseCssGradient } from './CssGradientParser';
+import { colorToNormalizedRgba } from './utils';
+
+export type Color = number | string | Float32Array | number[];
+
+export type ColorStop = {
+    offset: number;
+    color: Color;
+    alpha: number;
+};
+
+export type DefaultOptions = {
+    type: number;
+    stops: ColorStop[];
+    angle?: number;
+    alpha?: number;
+    maxColors?: number;
+};
+
+export type CssOptions = {
+    css: string;
+    alpha?: number;
+    maxColors?: number;
+};
+
+const ANGLE_OFFSET = 90; // align degrees with CSS
+
+function sortColorStops(stops: ColorStop[]): ColorStop[]
+{
+    return [...stops].sort((a, b) => a.offset - b.offset);
+}
+
+/**
+ * Render a colored gradient.<br>
+ * ![original](../tools/screenshots/dist/original.png)![filter](../tools/screenshots/dist/color-gradient.png)
+ *
+ * @class
+ * @extends PIXI.Filter
+ * @see {@link https://www.npmjs.com/package/@pixi/filter-color-gradient|@pixi/filter-color-gradient}
+ * @see {@link https://www.npmjs.com/package/pixi-filters|pixi-filters}
+ */
+class ColorGradientFilter extends Filter
+{
+    /** Gradient types */
+    static readonly LINEAR = 0;
+    static readonly RADIAL = 1;
+    static readonly CONIC = 2;
+
+    /** Default constructor options */
+    public static readonly defaults: DefaultOptions = {
+        type: ColorGradientFilter.LINEAR,
+        stops: [
+            { offset: 0.0, color: 0xff0000, alpha: 1.0 },
+            { offset: 1.0, color: 0x0000ff, alpha: 1.0 },
+        ],
+        alpha: 1.0,
+        angle: 90.0,
+        maxColors: 0,
+    };
+
+    private _stops: ColorStop[] = [];
+
+    /**
+   * @param {DefaultOptions | CssOptions} [options]
+   * @param {number} [options.alpha=1.0] - Alpha value
+   * @param {number} [options.maxColors=0] - Maximum number of colors to render (0 = disabled)
+   */
+    constructor(options?: DefaultOptions | CssOptions)
+    {
+        if (options && 'css' in options)
+        {
+            options = {
+                ...parseCssGradient(options.css || ''),
+                alpha: options.alpha,
+                maxColors: options.maxColors,
+            };
+        }
+
+        const options_ = { ...ColorGradientFilter.defaults, ...options };
+
+        if (!options_.stops || options_.stops.length < 2)
+        {
+            throw new Error('ColorGradientFilter requires at least 2 color stops.');
+        }
+
+        super(vertex, fragment);
+        this.autoFit = false;
+
+        Object.assign(this, options_);
+    }
+
+    get stops(): ColorStop[]
+    {
+        return this._stops;
+    }
+
+    set stops(stops: ColorStop[])
+    {
+        const sortedStops = sortColorStops(stops);
+
+        const colors = new Float32Array(sortedStops.length * 3);
+        const R = 0;
+        const G = 1;
+        const B = 2;
+
+        for (let i = 0; i < sortedStops.length; i++)
+        {
+            const color = colorToNormalizedRgba(sortedStops[i].color);
+            const indexStart = i * 3;
+
+            colors[indexStart + R] = color[R];
+            colors[indexStart + G] = color[G];
+            colors[indexStart + B] = color[B];
+        }
+
+        this.uniforms.uColors = colors;
+        this.uniforms.uOffsets = sortedStops.map((s) => s.offset);
+        this.uniforms.uAlphas = sortedStops.map((s) => s.alpha);
+        this.uniforms.uNumStops = sortedStops.length;
+        this._stops = sortedStops;
+    }
+
+    /**
+   * The type of gradient
+   * @default ColorGradientFilter.LINEAR
+   */
+    set type(value: number)
+    {
+        this.uniforms.uType = value;
+    }
+
+    get type(): number
+    {
+        return this.uniforms.uType;
+    }
+
+    /**
+   * The angle of the gradient in degrees
+   * @default 90
+   */
+    set angle(value: number)
+    {
+        this.uniforms.uAngle = value - ANGLE_OFFSET;
+    }
+
+    get angle(): number
+    {
+        return this.uniforms.uAngle + ANGLE_OFFSET;
+    }
+
+    /**
+   * The alpha value of the gradient (0-1)
+   * @default 1
+   */
+    set alpha(value: number)
+    {
+        this.uniforms.uAlpha = value;
+    }
+
+    get alpha(): number
+    {
+        return this.uniforms.uAlpha;
+    }
+
+    /**
+   * The maximum number of colors to render (0 = no limit)
+   * @default 0
+   */
+    set maxColors(value: number)
+    {
+        this.uniforms.uMaxColors = value;
+    }
+
+    get maxColors(): number
+    {
+        return this.uniforms.uMaxColors;
+    }
+}
+
+export { ColorGradientFilter };
+
---
+++ filters/color-gradient/src/colorGradient.frag
@@ -0,0 +1,127 @@
+const float PI = 3.1415926538;
+const float PI_2 = PI*2.;
+
+varying vec2 vTextureCoord;
+varying vec2 vFilterCoord;
+uniform sampler2D uSampler;
+
+const int TYPE_LINEAR = 0;
+const int TYPE_RADIAL = 1;
+const int TYPE_CONIC = 2;
+const int MAX_STOPS = 32;
+
+uniform int uNumStops;
+uniform float uAlphas[3*MAX_STOPS];
+uniform vec3 uColors[MAX_STOPS];
+uniform float uOffsets[MAX_STOPS];
+uniform int uType;
+uniform float uAngle;
+uniform float uAlpha;
+uniform int uMaxColors;
+
+struct ColorStop {
+    float offset;
+    vec3 color;
+    float alpha;
+};
+
+mat2 rotate2d(float angle){
+    return mat2(cos(angle), -sin(angle),
+    sin(angle), cos(angle));
+}
+
+float projectLinearPosition(vec2 pos, float angle){
+    vec2 center = vec2(0.5);
+    vec2 result = pos - center;
+    result = rotate2d(angle) * result;
+    result = result + center;
+    return clamp(result.x, 0., 1.);
+}
+
+float projectRadialPosition(vec2 pos) {
+    float r = distance(vFilterCoord, vec2(0.5));
+    return clamp(2.*r, 0., 1.);
+}
+
+float projectAnglePosition(vec2 pos, float angle) {
+    vec2 center = pos - vec2(0.5);
+    float polarAngle=atan(-center.y, center.x);
+    return mod(polarAngle + angle, PI_2) / PI_2;
+}
+
+float projectPosition(vec2 pos, int type, float angle) {
+    if (type == TYPE_LINEAR) {
+        return projectLinearPosition(pos, angle);
+    } else if (type == TYPE_RADIAL) {
+        return projectRadialPosition(pos);
+    } else if (type == TYPE_CONIC) {
+        return projectAnglePosition(pos, angle);
+    }
+
+    return pos.y;
+}
+
+void main(void) {
+    // current/original color
+    vec4 currentColor = texture2D(uSampler, vTextureCoord);
+
+    // skip calculations if gradient alpha is 0
+    if (0.0 == uAlpha) {
+        gl_FragColor = currentColor;
+        return;
+    }
+
+    // project position
+    float y = projectPosition(vFilterCoord, uType, radians(uAngle));
+
+    // check gradient bounds
+    float offsetMin = uOffsets[0];
+    float offsetMax = 0.0;
+
+    for (int i = 0; i < MAX_STOPS; i++) {
+        if (i == uNumStops-1){ // last index
+            offsetMax = uOffsets[i];
+        }
+    }
+
+    if (y  < offsetMin || y > offsetMax) {
+        gl_FragColor = currentColor;
+        return;
+    }
+
+    // limit colors
+    if (uMaxColors > 0) {
+        float stepSize = 1./float(uMaxColors);
+        float stepNumber = float(floor(y/stepSize));
+        y = stepSize * (stepNumber + 0.5);// offset by 0.5 to use color from middle of segment
+    }
+
+    // find color stops
+    ColorStop from;
+    ColorStop to;
+
+    for (int i = 0; i < MAX_STOPS; i++) {
+        if (y >= uOffsets[i]) {
+            from = ColorStop(uOffsets[i], uColors[i], uAlphas[i]);
+            to = ColorStop(uOffsets[i+1], uColors[i+1], uAlphas[i+1]);
+        }
+
+        if (i == uNumStops-1){ // last index
+            break;
+        }
+    }
+
+    // mix colors from stops
+    vec4 colorFrom = vec4(from.color * from.alpha, from.alpha);
+    vec4 colorTo = vec4(to.color * to.alpha, to.alpha);
+
+    float segmentHeight = to.offset - from.offset;
+    float relativePos = y - from.offset;// position from 0 to [segmentHeight]
+    float relativePercent = relativePos / segmentHeight;// position in percent between [from.offset] and [to.offset].
+
+    float gradientAlpha = uAlpha * currentColor.a;
+    vec4 gradientColor = mix(colorFrom, colorTo, relativePercent) * gradientAlpha;
+
+    // mix resulting color with current color
+    gl_FragColor = gradientColor + currentColor*(1.-gradientColor.a);
+}
